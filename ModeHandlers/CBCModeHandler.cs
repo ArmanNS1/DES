0fbcaf6ce924d5cb759ab446057cdb2b56734843
Arman <a.n004@yahoo.com>
2025-08-07 13:47:46 +0330

    Update CBCModeHandler.cs

--- ModeHandlers/CBCModeHandler.cs
+++ ModeHandlers/CBCModeHandler.cs
@@ -12,24 +12,18 @@ namespace DES.ModeHandlers
             ValidateBlockSize(plaintext);
             var result = new byte[plaintext.Length];
 
-            // Initialize previous block with IV
             BitArray previousBlock = new BitArray(InitializationVector);
 
             for (int i = 0; i < plaintext.Length; i += 8)
             {
-                // Get current plaintext block
                 BitArray currentBlock = ConvertToBitArray(plaintext, i);
 
-                // XOR with previous ciphertext block (or IV for first block)
                 currentBlock.Xor(previousBlock);
 
-                // Encrypt the XORed block
                 BitArray encryptedBlock = DESEncryption.EncryptBlock(currentBlock, RoundKeys);
 
-                // Store the result
                 CopyBitArrayToBytes(encryptedBlock, result, i);
 
-                // Update previous block for next iteration
                 previousBlock = encryptedBlock;
             }
 
@@ -41,27 +35,20 @@ namespace DES.ModeHandlers
             ValidateBlockSize(ciphertext);
             var result = new byte[ciphertext.Length];
 
-            // Initialize previous block with IV
             BitArray previousBlock = new BitArray(InitializationVector);
 
             for (int i = 0; i < ciphertext.Length; i += 8)
             {
-                // Get current ciphertext block
                 BitArray currentBlock = ConvertToBitArray(ciphertext, i);
-
-                // Store current block for next iteration (before decryption)
+                
                 BitArray nextPreviousBlock = new BitArray(currentBlock);
 
-                // Decrypt the current block
                 BitArray decryptedBlock = DESEncryption.DecryptBlock(currentBlock, RoundKeys);
 
-                // XOR with previous ciphertext block (or IV for first block)
                 decryptedBlock.Xor(previousBlock);
 
-                // Store the result
                 CopyBitArrayToBytes(decryptedBlock, result, i);
 
-                // Update previous block for next iteration
                 previousBlock = nextPreviousBlock;
             }
 
